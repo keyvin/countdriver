7ed548d3d63d1370cf7b82d4cc086379ff531107
Hacker Space <lvl1@localhost>
2015-10-11 17:09:25 +0000

    Added command line options for count  up and coutn down

--- time.py
+++ time.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+
 import datetime
 import random
 import time
 import RPi.GPIO as gpio
+import sys
+
 
 def getDirection(direction=-1):
         #  switch directions (positive if initial)
@@ -14,14 +18,14 @@ def getDirection(direction=-1):
         #  convert timestring to bcd, output to timer
 def makebcd(to_bcd):
         #  set pin numbers
-         #  split time into numbers
+        #  split time into numbers
     just_nums = ''.join(to_bcd.split()[1].split(':'))
     nums = list("00"+just_nums)
     bcd = []
     for i in nums:
         tmp = format(int(i),"#010b")
         bcd.append(tmp[6:])
-    strobe(bcd)
+    return bcd
 
         #  output time to display            
         #  initialize time, loop execution
@@ -43,7 +47,49 @@ def strobe(bcd):
         time.sleep(.000001)
 
 
-def main():
+def countmain(time_set, count_direction):
+    direction = datetime.timedelta(seconds = count_direction)
+    if count_direction == -1:    
+        our_time = time_set
+    if count_direction == 1:
+        our_time == datetime.datetime(time_set.day)
+    while True:
+        #  wait one second
+        
+        #  count second
+        
+        #  convert string to bcd
+        print (our_time)
+        flag = True  
+
+        #count down
+        delta = datetime.timedelta()
+        if count_direction == -1:
+            #strobe one second
+            while flag:
+                then = datetime.datetime.now()
+                makebcd(str(our_time))
+                now = datetime.datetime.now()
+                delta = delta + (now - then)
+                if delta.seconds == 1:
+                    flag = False
+        #update time. 
+            our_time = our_time + direction
+            if our_time.hour == 0 and our_time.minute == 0 and our_time.second == 0:
+                direction = datetime.timedelta()
+
+        if count_direction == 1:
+            while flag:
+                then = datetime.datetime.now()
+                makebcd(str(our_time))
+                now = datetime.datetime.now()
+                delta = delta +(now - then)
+                if delta.seconds == 1:
+                    flag = False
+            if our_time == time_set:
+                direction = datetime.timedelta()
+
+def ranmain():
         #  determine time values
     random.seed(time.time())
     our_time = datetime.datetime(2000,1,1,int(random.random()*24), int(random.random()*60), int(random.random()*60))
@@ -60,12 +106,15 @@ def main():
         our_time = our_time + datetime.timedelta(0,time_dir)
         print (str(our_time) + " " + str(count) + " " + str(ticks))
         #  convert string to bcd
+        bcd = makebcd(str(our_time))
         flag = True  
+        delta = datetime.timedelta()
         while flag:
             then = datetime.datetime.now()
-            makebcd(str(our_time))
+            strobe(bcd)
             now = datetime.datetime.now()
-            delta = now - then
+            delta = delta + (now - then)
+#            print(delta)
             if delta.seconds == 1:
                 flag = False
 
@@ -78,22 +127,39 @@ def main():
         #   reinitialize length of timer
             ticks = int(random.random()*45)+15
         #   reinitialize counter
-            count = 0
-            
-        
-    
-        #  initialize display and output pins
-gpio.setmode(gpio.BOARD)
-gpio.setwarnings(False)
-outpins = [3, 5, 7, 11, 13, 15, 19, 21, 29,31,33,35, 37, 23]
-plus_minus = 37
-over_under = 23
-disp_pins = [3, 5, 7, 11, 13, 15, 19, 21]
-for i in outpins:
-    gpio.setup(i, gpio.OUT)
-    
-for i in disp_pins:
-    gpio.setup(i, gpio.OUT, initial=gpio.LOW)
-    
+            count = 0                   
+         #  initialize display and output pins
 
-main()
+if __name__ == '__main__':
+    gpio.setmode(gpio.BOARD)
+    gpio.setwarnings(False)
+    outpins = [3, 5, 7, 11, 13, 15, 19, 21, 29,31,33,35, 37, 23]
+    plus_minus = 37
+    over_under = 23
+    disp_pins = [3, 5, 7, 11, 13, 15, 19, 21]
+    for i in outpins:
+        gpio.setup(i, gpio.OUT)
+    
+    for i in disp_pins:
+        gpio.setup(i, gpio.OUT, initial=gpio.LOW)
+    print (sys.argv[0])
+    if len(sys.argv) == 1:
+        print ("Usage: time.py random - random countdown\n time.py direction DD:HH:MM:SS\n direction is 1 for count up, -1 for countdown")
+        sys.exit(0)
+    
+    if sys.argv[1] == 'random':
+        ranmain()
+    
+    if sys.argv[1] == 'count':
+        direction = int(sys.argv[2])
+        time = sys.argv[3] 
+        #time format 
+        #DD:HH:MM:SS
+        #split time to prep for date time obj
+        i_time = time.split(':')
+        for i in range(len(i_time)):
+            i_time[i] = int(i_time[i])
+
+        time_obj = datetime.datetime(2000, 10, i_time[0], i_time[1], i_time[2], i_time[3])
+        countmain(time_obj, direction)
+
